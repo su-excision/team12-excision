9747f30fea56bf4988ad465c95dd6dc2af84e260
Eduardo Sousa Silva <esousasilva@seattleu.edu>
2023-10-02 19:54:52 -0700

    lab demo test

--- src/Pages/Error.cshtml.cs
+++ src/Pages/Error.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 //adding a new comment
+//adding a new comment[2]
 namespace ContosoCrafts.WebSite.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
