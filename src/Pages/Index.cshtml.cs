ade1d7edc78162eb4b2f92d10f3b28e2b5ec8a7d
Padmaja Buggaveeti <59131010+PjBuggaveeti@users.noreply.github.com>
2023-10-04 13:38:07 -0700

    Added name Padmaja to IndexModel Comment in Index.cshtml.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -9,7 +9,7 @@ using ContosoCrafts.WebSite.Services;
 namespace ContosoCrafts.WebSite.Pages
 {
     /// <summary>
-    /// Mike Koenig
+    /// Padmaja
     /// </summary>
     public class IndexModel : PageModel
     {
