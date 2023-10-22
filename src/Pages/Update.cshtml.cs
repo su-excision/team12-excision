ba7461ab07335b50e21b0edd469c181cc416014e
Emily Fang Bazar <emilyfbazar@gmail.com>
2023-10-21 18:13:21 -0700

    Create UpdateModel for Update page on Update.cshtml.cs

--- src/Pages/Update.cshtml.cs
+++ src/Pages/Update.cshtml.cs
@@ -1,19 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+﻿using System.Collections.Generic;
 
-namespace ContosoCrafts.WebSite.Pages
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+
+namespace ContosoCrafts.WebSite.Pages.Update
 {
+    /// <summary>
+    /// Update page will return Product data such as Name and Description
+    /// </summary>
     public class UpdateModel : PageModel
     {
-        private readonly ILogger<UpdateModel> _logger;
-
-        public UpdateModel(ILogger<UpdateModel> logger)
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="productService"></param>
+        public UpdateModel(JsonFileProductService productService)
         {
-            _logger = logger;
+            ProductService = productService;
         }
 
+        // Data Service
+        public JsonFileProductService ProductService { get; }
+
+        // Collection of the Data
+        public IEnumerable<ProductModel> Products { get; private set; }
+
+        /// <summary>
+        /// REST OnGet, return all data
+        /// </summary>
         public void OnGet()
         {
+            Products = ProductService.GetProducts();
         }
     }
 }
