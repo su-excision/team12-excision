bed3bc8363b400f1c405a3658ff8422e2961f7a8
Emily Fang Bazar <emilyfbazar@gmail.com>
2023-10-21 17:41:32 -0700

    Added new file for Update page modeling Razor's PageModel on Update.cshtml.cs

---
+++ src/Pages/Update.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    public class UpdateModel : PageModel
+    {
+        private readonly ILogger<UpdateModel> _logger;
+
+        public UpdateModel(ILogger<UpdateModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
