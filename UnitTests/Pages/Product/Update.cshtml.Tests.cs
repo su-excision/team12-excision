a2c34e4cc7542f3f0f77ae429ee7164d0eb9341a
Emily Fang Bazar <emilyfbazar@gmail.com>
2023-10-23 22:33:39 -0700

    Added Product getter unit test for Update.cshtml.cs

--- UnitTests/Pages/Product/Update.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Update.cshtml.Tests.cs
@@ -18,6 +18,7 @@ using ContosoCrafts.WebSite.Pages.Product;
 using ContosoCrafts.WebSite.Services;
 using ContosoCrafts.WebSite.Pages.Product.Update;
 using ContosoCrafts.WebSite.Models;
+using System.Collections.Generic;
 
 namespace UnitTests.Pages.Product.Update
 {
@@ -110,5 +111,44 @@ namespace UnitTests.Pages.Product.Update
             Assert.AreEqual(expectedProductService, actualProductService);
         }
         #endregion ProductService
+
+        #region ProductProperty getter
+        [Test]
+        public void ProductProperty_Get_Should_Return_Expected_Value()
+        {
+            // Arrange
+            var mockWebHostEnvironment = new Mock<IWebHostEnvironment>();
+            mockWebHostEnvironment.Setup(m => m.EnvironmentName).Returns("Hosting:UnitTestEnvironment");
+            mockWebHostEnvironment.Setup(m => m.WebRootPath).Returns("../../../../src/bin/Debug/net7.0/wwwroot");
+            mockWebHostEnvironment.Setup(m => m.ContentRootPath).Returns("./data/");
+
+            var expectedProductService = new JsonFileProductService(mockWebHostEnvironment.Object);
+            var updateModel = new UpdateModel(expectedProductService);
+            var expectedProduct = new ProductModel
+            {
+                Id = "2",
+                Name = "Heracross",
+                Description = "A bug type Pokémon with sharp claws on his feet.",
+                Value = "$14.99",
+                Rarity = "Uncommon",
+                Availability = 8,
+                Type = new List<string> {"Bug", "Fighting"}
+
+            };
+
+            // Act
+            updateModel.Product = expectedProduct;
+            var actualProduct = updateModel.Product;
+
+            // Assert
+            Assert.AreEqual(expectedProduct.Id, actualProduct.Id);
+            Assert.AreEqual(expectedProduct.Name, actualProduct.Name);
+            Assert.AreEqual(expectedProduct.Description, actualProduct.Description);
+            Assert.AreEqual(expectedProduct.Value, actualProduct.Value);
+            Assert.AreEqual(expectedProduct.Rarity, actualProduct.Rarity);
+            Assert.AreEqual(expectedProduct.Availability, actualProduct.Availability);
+            CollectionAssert.AreEqual(expectedProduct.Type, actualProduct.Type);
+        }
+        #endregion ProductProperty getter
     }
 }
