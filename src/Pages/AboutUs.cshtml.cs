145cb2f19863446417d589103225b39adee5b062
Andrew Asplund <asplunda@seattleu.edu>
2023-10-29 10:21:58 -0700

    Added comments to AboutUs.cshtml.cs

--- src/Pages/AboutUs.cshtml.cs
+++ src/Pages/AboutUs.cshtml.cs
@@ -3,15 +3,29 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+
+    /// <summary>
+    /// Page Model for the About Us page of the website.
+    /// </summary>
     public class AboutUsModel : PageModel
     {
+        /// <summary>
+        /// Logger used for creating logs of site operation.
+        /// </summary>
         private readonly ILogger<AboutUsModel> _logger;
 
+        /// <summary>
+        /// Constructor for the About Us Page Model.
+        /// </summary>
+        /// <param name="logger">the ILogger used for the AboutUs page on creation.</param>
         public AboutUsModel(ILogger<AboutUsModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// Method that is called when the page is loaded (after a GET request).
+        /// </summary>
         public void OnGet()
         {
         }
