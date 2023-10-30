ccf8c0563d5167a8dc619f552c459fbeedfd33b1
Andrew Asplund <asplunda@seattleu.edu>
2023-10-29 20:22:58 -0700

    Added comments to Privacy.cshtml.cs

--- src/Pages/Privacy.cshtml.cs
+++ src/Pages/Privacy.cshtml.cs
@@ -3,15 +3,28 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Page Model for the Privacy page of the website.
+    /// </summary>
     public class PrivacyModel : PageModel
     {
+        /// <summary>
+        /// Logger used for creating logs of site operation.
+        /// </summary>
         private readonly ILogger<PrivacyModel> _logger;
 
+        /// <summary>
+        /// Constructor for the Privacy Page Model.
+        /// </summary>
+        /// <param name="logger">the ILogger used for the AboutUs page on creation.</param>
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// Method that is called when the page is loaded (after a GET request).
+        /// </summary>
         public void OnGet()
         {
         }
