6ac599fbded0ddd4996ece646e6c206dcfa49066
Emily Fang Bazar <emilyfbazar@gmail.com>
2023-10-29 23:27:59 -0700

    Create Delete file with Delete.cshtml.cs

---
+++ src/Pages/Product/Delete.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace ContosoCrafts.WebSite.Pages.Product.Delete
+{
+    /// <summary>
+    /// Update page will return Product data such as Name and Description
+    /// </summary>
+    public class DeleteModel : PageModel
+    {
+        /// <summary>
+        /// ProductService for retrieving products from the datastore.
+        /// </summary>
+        private readonly JsonFileProductService _productService;
+
+        /// <summary>
+        /// Constructor for the Delete Page Model.
+        /// </summary>
+        /// <param name="productService">ProductService for retrieivng products
+        /// from the datastore.</param>
+        public DeleteModel(JsonFileProductService productService)
+        {
+            _productService = productService;
+        }
+
+        /// <summary>
+        /// The product data to be displayed on the Page.
+        /// </summary>
+        public ProductModel Product;
+
+        /// <summary>
+        /// Method that is called when the page is loaded (after a GET request). Loads
+        /// the desired Product from the datastore based on the product Id passed to it.
+        /// </summary>
+        /// <param name="id">the Product Id for the desired Product to load.</param>
+        public void OnGet(string id)
+        {
+            Product = _productService.GetProducts().FirstOrDefault(m => m.Id.Equals(id));
+        }
+    }
+}
+
+
+
