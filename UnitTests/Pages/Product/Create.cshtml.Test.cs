8538c1d83a44d4492192a2d2a5674be8f59f06ac
Eduardo Sousa Silva <esousasilva@seattleu.edu>
2023-10-30 22:44:43 -0700

    Added 100% coverage to Unit test for Create.cshtml.cs file

---
+++ UnitTests/Pages/Product/Create.cshtml.Test.cs
@@ -0,0 +1,187 @@
+﻿using System.Linq;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Product;
+using ContosoCrafts.WebSite.Services;
+using ContosoCrafts.WebSite.Pages.Product.Create;
+using ContosoCrafts.WebSite.Models;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using ContosoCrafts.WebSite.Pages.Product.Update;
+
+namespace UnitTests.Pages.Product.Create
+{
+    internal class CreateTests
+    {
+        #region TestSetup
+        public static IUrlHelperFactory urlHelperFactory;
+        public static DefaultHttpContext httpContextDefault;
+        public static IWebHostEnvironment webHostEnvironment;
+        public static ModelStateDictionary modelState;
+        public static ActionContext actionContext;
+        public static EmptyModelMetadataProvider modelMetadataProvider;
+        public static ViewDataDictionary viewData;
+        public static TempDataDictionary tempData;
+        public static PageContext pageContext;
+
+        public static IndexModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            httpContextDefault = new DefaultHttpContext()
+            {
+                //RequestServices = serviceProviderMock.Object,
+            };
+
+            modelState = new ModelStateDictionary();
+
+            actionContext = new ActionContext(httpContextDefault, httpContextDefault.GetRouteData(), new PageActionDescriptor(), modelState);
+
+            modelMetadataProvider = new EmptyModelMetadataProvider();
+            viewData = new ViewDataDictionary(modelMetadataProvider, modelState);
+            tempData = new TempDataDictionary(httpContextDefault, Mock.Of<ITempDataProvider>());
+
+            pageContext = new PageContext(actionContext)
+            {
+                ViewData = viewData,
+            };
+
+            var mockWebHostEnvironment = new Mock<IWebHostEnvironment>();
+            mockWebHostEnvironment.Setup(m => m.EnvironmentName).Returns("Hosting:UnitTestEnvironment");
+            mockWebHostEnvironment.Setup(m => m.WebRootPath).Returns("../../../../src/bin/Debug/net7.0/wwwroot");
+            mockWebHostEnvironment.Setup(m => m.ContentRootPath).Returns("./data/");
+
+            var MockLoggerDirect = Mock.Of<ILogger<IndexModel>>();
+            JsonFileProductService productService;
+
+            productService = new JsonFileProductService(mockWebHostEnvironment.Object);
+
+            pageModel = new IndexModel(productService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Should_Return_Products()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(10, pageModel.Products.ToList().Count);
+        }
+
+        [Test]
+        public void OnGet_Should_Populate_Product_And_AvailableTypes()
+        {
+            // Arrange
+            var productId = "ID";
+            var mockWebHostEnvironment = new Mock<IWebHostEnvironment>();
+            mockWebHostEnvironment.Setup(m => m.EnvironmentName).Returns("Hosting:UnitTestEnvironment");
+            mockWebHostEnvironment.Setup(m => m.WebRootPath).Returns("../../../../src/bin/Debug/net7.0/wwwroot");
+            mockWebHostEnvironment.Setup(m => m.ContentRootPath).Returns("./data/");
+
+            var productService = new JsonFileProductService(mockWebHostEnvironment.Object);
+            var createModel = new CreateModel(productService);
+
+            // Act
+            createModel.OnGet();
+
+            // Assert
+            Assert.IsNotNull(createModel.Product);
+            Assert.AreEqual(productId, "ID");
+            Assert.AreEqual("Enter the Name of the Pokemon", createModel.Product.Name);
+            Assert.AreEqual(0f, createModel.Product.Value);
+            Assert.AreEqual("Enter the Expansion", createModel.Product.Expansion);
+            Assert.AreEqual("Enter the Rarity", createModel.Product.Rarity);
+            Assert.AreEqual(0, createModel.Product.Availability);
+            Assert.AreEqual("https://i.imgflip.com/4u072l.png", createModel.Product.Image);
+            Assert.AreEqual("Enter the Description", createModel.Product.Description);
+            Assert.AreEqual(null, createModel.Product.Ratings);
+            CollectionAssert.AreEqual(
+                new List<string> { "grass", "lightning", "darkness", "fairy", "fire", "psychic", "metal", "dragon", "water", "fighting", "colorless" },
+                createModel.AvailableTypes);
+        }
+        #endregion OnGet
+
+
+        #region OnPost
+        [Test]
+        public void OnPost_Valid_Model_Should_Return_Redirect_To_PageResult()
+        {
+            // Arrange
+            var mockWebHostEnvironment = new Mock<IWebHostEnvironment>();
+            mockWebHostEnvironment.Setup(m => m.EnvironmentName).Returns("Hosting:UnitTestEnvironment");
+            mockWebHostEnvironment.Setup(m => m.WebRootPath).Returns("../../../../src/bin/Debug/net7.0/wwwroot");
+            mockWebHostEnvironment.Setup(m => m.ContentRootPath).Returns("./data/");
+
+            var testWebHostEnvironment = mockWebHostEnvironment.Object;
+            var productService = new JsonFileProductService(testWebHostEnvironment);
+            var createModel = new CreateModel(productService);
+
+            createModel.ModelState.AddModelError("PropertyName", "Error");
+
+            // Act
+            var result = createModel.OnPost();
+
+            // Assert
+            Assert.IsInstanceOf<PageResult>(result);
+        }
+
+        [Test]
+        public void OnPost_Valid_Model_Should_Create_Product_And_Redirect()
+        {
+            // Arrange
+            var mockWebHostEnvironment = new Mock<IWebHostEnvironment>();
+            mockWebHostEnvironment.Setup(m => m.EnvironmentName).Returns("Hosting:UnitTestEnvironment");
+            mockWebHostEnvironment.Setup(m => m.WebRootPath).Returns("../../../../src/bin/Debug/net7.0/wwwroot");
+            mockWebHostEnvironment.Setup(m => m.ContentRootPath).Returns("./data/");
+
+            var testWebHostEnvironment = mockWebHostEnvironment.Object;
+            var productService = new JsonFileProductService(testWebHostEnvironment);
+            var createModel = new CreateModel(productService);
+            var validProduct = new ProductModel
+            {
+                Id = "SXA12",
+                Name = "CATERPIE",
+                Description = "Caterpie is a worm-like Pokémon that is mainly green in color with a tan underside. Just below its head are four, tiny legs that are used only for movement.",
+                Value = 9.99f,
+                Rarity = "Uncommon",
+                Availability = 19,
+                Type = new List<string> { "Bug"}
+            };
+
+            createModel.ModelState.Clear();
+            createModel.Product = validProduct;
+
+            // Act
+            var result = createModel.OnPost();
+
+            // Assert
+            Assert.IsInstanceOf<RedirectToPageResult>(result);
+            var redirectToPageResult = (RedirectToPageResult)result;
+            Assert.AreEqual("./Index", redirectToPageResult.PageName);
+        }
+        #endregion OnPost
+    }
+}
