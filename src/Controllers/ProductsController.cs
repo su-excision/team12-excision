5bc365bd4f2c06fbbca696909a5bf6144e2a1a07
Emily Fang Bazar <emilyfbazar@gmail.com>
2023-11-04 18:29:36 -0700

    Added comments to ProductsController.cs

--- src/Controllers/ProductsController.cs
+++ src/Controllers/ProductsController.cs
@@ -7,23 +7,43 @@ using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Controllers
 {
+    /// <summary>
+    /// Controller for managing products
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class ProductsController : ControllerBase
     {
+        /// <summary>
+        /// Initializes a new instance of the ProductsController class
+        /// </summary>
+        /// <param name="productService">The JSON file product service.</param>
         public ProductsController(JsonFileProductService productService)
         {
             ProductService = productService;
         }
 
+        /// <summary>
+        /// Gets the product service
+        /// </summary>
+        /// <returns>The product service instance.</returns>
         public JsonFileProductService ProductService { get; }
 
+        /// <summary>
+        /// Gets a list of products
+        /// </summary>
+        /// <returns>An IEnumerable of ProductModel objects.</returns>
         [HttpGet]
         public IEnumerable<ProductModel> Get()
         {
             return ProductService.GetProducts();
         }
 
+        /// <summary>
+        /// Adds a rating to a product
+        /// </summary>
+        /// <param name="request">The rating request.</param>
+        /// <returns>An IActionResult indicating the result of the operation.</returns>
         [HttpPatch]
         public ActionResult Patch([FromBody] RatingRequest request)
         {
@@ -32,6 +52,9 @@ namespace ContosoCrafts.WebSite.Controllers
             return Ok();
         }
 
+        /// <summary>
+        /// Represents a rating request that has a ProductId and a Rating
+        /// </summary>
         public class RatingRequest
         {
             public string ProductId { get; set; }
