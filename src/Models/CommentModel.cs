f383e3ab7b0a7c6a21eaa867371ee99fabf4942e
Eduardo Sousa Silva <esousasilva@seattleu.edu>
2023-11-09 16:48:23 -0800

    Created the CommentModel.cs file

---
+++ src/Models/CommentModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ContosoCrafts.WebSite.Models
+{
+    public class CommentModel
+    {
+        // The ID for this comment, use a Guid so it is always unique
+        public string Id { get; set; } = System.Guid.NewGuid().ToString();
+
+        // The Comment getter and setter
+        public string Comment { get; set; }
+    }
+}
