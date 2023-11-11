745abc55260d519c75235aa4da9eceea2e2a9321
Andrew Asplund <asplunda@seattleu.edu>
2023-11-10 16:32:58 -0800

    Updated coments in CommentModel.cs

--- src/Models/CommentModel.cs
+++ src/Models/CommentModel.cs
@@ -1,11 +1,19 @@
 ﻿namespace ContosoCrafts.WebSite.Models
 {
+    /// <summary>
+    /// Model for comments posted by users. Contains a unique
+    /// identifier and the text of the comment.
+    /// </summary>
     public class CommentModel
     {
-        // The ID for this comment, use a Guid so it is always unique
+        /// <summary>
+        /// Unique Identifier for the comment.
+        /// </summary>
         public string Id { get; set; } = System.Guid.NewGuid().ToString();
 
-        // The Comment getter and setter
+        /// <summary>
+        /// String representation of a comment posted by a user.
+        /// </summary>
         public string Comment { get; set; }
     }
 }
