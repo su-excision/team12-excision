974fd7a80f63a4b58f089693b5b5239d17745eee
Andrew Asplund <asplunda@seattleu.edu>
2023-11-12 20:58:25 -0800

    Added SelectProduct test to ProductList.razor.Tests.cs

--- UnitTests/Components/ProductList.razor.Tests.cs
+++ UnitTests/Components/ProductList.razor.Tests.cs
@@ -10,6 +10,7 @@ using Bunit;
 using NUnit.Framework;
 using ContosoCrafts.WebSite.Components;
 using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace UnitTests.Components
 {
@@ -44,6 +45,36 @@ namespace UnitTests.Components
         }
 
         #endregion ProductList
+
+        #region SelectProduct
+
+        /// <summary>
+        /// Tests to see that the More Info button brings up the modal containing the correct product data.
+        /// </summary>
+        [Test]
+        public void SelectProduct_Valid_ValidId_Should_ReturnContent()
+        {
+            // Arrange
+            Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
+            const string TestButtonId = "MoreInfo_XY-088";
+            const string TestDescription = "When its huge eyes waver, it sings a mysteriously soothing melody that lulls its enemies to sleep.";
+
+            var cut = RenderComponent<ProductList>();
+            var testButton = cut.FindAll("Button").First(element => element.OuterHtml.Contains(TestButtonId));
+
+            // Act
+
+            testButton.Click();
+            var markup = cut.Markup;
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, markup.Contains(TestDescription));
+
+        }
+
+        #endregion SelectProduct
 
     }
 }
