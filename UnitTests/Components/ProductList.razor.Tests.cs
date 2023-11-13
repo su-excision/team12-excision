9ab5b4bb53128a53c964ff2333c15da12c6cba56
Andrew Asplund <asplunda@seattleu.edu>
2023-11-12 18:45:47 -0800

    Created ProductList.razor.Tests.cs, added UT for ProductList

---
+++ UnitTests/Components/ProductList.razor.Tests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Bunit;
+using NUnit.Framework;
+using ContosoCrafts.WebSite.Components;
+using ContosoCrafts.WebSite.Services;
+
+namespace UnitTests.Components
+{
+    public class ProductListTests : BunitTestContext
+    {
+        #region ProductList
+
+        [Test]
+        public void ProductList_Default_Rendered_Should_ReturnContent()
+        {
+            // Arrange
+            const string TestString = "Pineco";
+            Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
+
+
+            // Act
+            var cut = RenderComponent<ProductList>();
+
+
+            // Reset
+            var result = cut.Markup;
+
+
+            // Assert
+            Assert.AreEqual(true, result.Contains(TestString));
+        }
+
+        #endregion ProductList
+
+    }
+}
