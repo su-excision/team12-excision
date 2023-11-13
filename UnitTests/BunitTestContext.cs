c20cca9a12608342047ca391c49e620427b64cb9
Andrew Asplund <asplunda@seattleu.edu>
2023-11-12 18:44:52 -0800

    Added unitTestContext.cs

---
+++ UnitTests/BunitTestContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Bunit;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    public abstract class BunitTestContext : TestContextWrapper
+    {
+        [SetUp]
+        public void Setup() => TestContext = new Bunit.TestContext();
+
+        [TearDown]
+        public void TearDown() => TestContext?.Dispose();
+    }
+}
