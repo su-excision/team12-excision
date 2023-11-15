9e12fbf49887c5c037da778e3686f39fa2a8b279
Eduardo Sousa Silva <esousasilva@seattleu.edu>
2023-11-14 19:49:43 -0800

    Added the CommentModel.csTests.cs file

---
+++ UnitTests/Models/CommentModel.cs.Tests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Models
+{
+    internal class CommentModel
+    {
+        [Test]
+        public void Constructor_Valid_NewInstance_Should_AddGuid()
+        {
+            // Arrange
+            var testProduct = new TestProductBuilder().WithComment("12","test");
+            const string SerializedOutput = "{\"Id\":\"TST-000\",\"Name\":\"Test Card\",\"Value\":99.99,\"Expansion\":\"Test Expansion\",\"Rarity\":\"Unique\",\"Availability\":999,\"Type\":[],\"img\":\"https://assets.pokemon.com/assets/cms2/img/cards/web/SMA/SMA_EN_SV46.png\",\"Description\":\"A Test Card used for Testing\",\"Ratings\":[],\"CommentList\":[{\"Id\":\"12\",\"Comment\":\"test\"}]}";
+
+            // Act
+            string result = testProduct.Build().ToString();
+
+            // Assert
+            Assert.AreEqual(SerializedOutput, result);
+        }
+    }
+}
