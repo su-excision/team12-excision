cbd3119c653c1648174320fc5619bdba274ab5da
Andrew Asplund <asplunda@seattleu.edu>
2023-11-17 09:10:16 -0800

    Added TestConstants.cs

---
+++ UnitTests/TestConstants.cs
@@ -0,0 +1,26 @@
+namespace UnitTests
+{
+    /// <summary>
+    /// Static class for constants used in testing.
+    /// </summary>
+    public static class TestConstants
+    {
+        /// <summary>
+        /// Expected number of products in the JSON datastore file.
+        /// </summary>
+        public const int ExpectedProductCount = 12;
+
+        /// <summary>
+        /// Expected Product Id for the first product in the JSON
+        /// datastore file.
+        /// </summary>
+        public const string ExpectedFirstProductId = "SVI-001";
+
+        /// <summary>
+        /// Expected Product Id for the last product in the JSON
+        /// datastore file.
+        /// </summary>
+        public const string ExpectedLastProductId = "SSH-186";
+    }
+
+};
