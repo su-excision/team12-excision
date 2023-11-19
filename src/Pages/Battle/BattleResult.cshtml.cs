ea503252359c1634d2cb6268c65a1348b6c7669c
Emily Fang Bazar <emilyfbazar@gmail.com>
2023-11-18 22:46:11 -0800

    Added randomized BattleOutcomeMessage to BattleResult.cshtml.cs

--- src/Pages/Battle/BattleResult.cshtml.cs
+++ src/Pages/Battle/BattleResult.cshtml.cs
@@ -1,9 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
 
-namespace ContosoCrafts.WebSite.Pages
+namespace ContosoCrafts.WebSite.Pages.Battle
 {
     public class BattleResultModel : PageModel
     {
-        // fill in
+        public List<string> SelectedPokemonNames { get; set; }
+        public string BattleOutcomeMessage { get; set; }
+
+        public void OnGet()
+        {
+          
+        }
+
+        public void OnPost()
+        {
+            Random random = new Random();
+            if (random.Next(2) == 0)
+            {
+                BattleOutcomeMessage = "Congratulations! You won the battle!";
+            }
+            else
+            {
+                BattleOutcomeMessage = "Sorry, you were utterly defeated.";
+            }
+        }
     }
 }
