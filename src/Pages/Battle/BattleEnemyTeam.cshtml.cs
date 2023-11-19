76056498733860562087203e16fc6d1d442a5579
Emily Fang Bazar <emilyfbazar@gmail.com>
2023-11-18 22:43:32 -0800

    Added BattleEnemyTeam page on BattleEnemyTeam.cshtml

---
+++ src/Pages/Battle/BattleEnemyTeam.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ContosoCrafts.WebSite.Pages.Battle
+{
+    public class BattleEnemyTeamModel : PageModel
+    {
+        private readonly JsonFileProductService productService;
+
+        public IEnumerable<ProductModel> EnemyPokemonTeam { get; private set; }
+
+        public BattleEnemyTeamModel(JsonFileProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        public void OnGet()
+        {
+            EnemyPokemonTeam = productService.GetProducts()
+                .Where(p => p.CardCategory == CardType.Pokemon)
+                .OrderBy(p => Guid.NewGuid())
+                .Take(5)
+                .ToList();
+        }
+    }
+}
