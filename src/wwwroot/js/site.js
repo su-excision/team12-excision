c03ed9f95048b5f3e6eb4150481d6cbd2be22616
Emily Fang Bazar <emilyfbazar@gmail.com>
2023-11-18 23:18:33 -0800

    Added function to play different audio based on battle outcome to site.js

--- src/wwwroot/js/site.js
+++ src/wwwroot/js/site.js
@@ -1,4 +1,21 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your Javascript code.
+/// Plays corresponding audio based on whether victory or defeat message is present
+document.addEventListener('DOMContentLoaded', function () {
+    var audio;
+
+    // Check if the page contains a specific message of the battle outcome
+    var victoryElement = document.getElementById('victory-message');
+    var defeatElement = document.getElementById('defeat-message');
+
+    if (victoryElement) {
+        audio = new Audio('/audio/pokemon-victory.mp3');
+    } else if (defeatElement) {
+        audio = new Audio('/audio/pokemon-defeat.mp3');
+    }
+
+    if (audio) {
+        audio.play();
+    }
+});
