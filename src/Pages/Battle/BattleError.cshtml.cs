19fc083ec6494ff4f98802fca0edab76794e81ba
Emily Fang Bazar <emilyfbazar@gmail.com>
2023-11-19 22:49:20 -0800

    Removed logger from BattleError.cshtml.cs

--- src/Pages/Battle/BattleError.cshtml.cs
+++ src/Pages/Battle/BattleError.cshtml.cs
@@ -1,20 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
     public class BattleErrorModel : PageModel
     {
-        private readonly ILogger<BattleErrorModel> _logger;
-
-        public BattleErrorModel(ILogger<BattleErrorModel> logger)
-        {
-            _logger = logger;
-        }
-
         public void OnGet()
         {
-            _logger.LogError("Invalid number of selected Pokémon in the battle.");
         }
     }
 }
